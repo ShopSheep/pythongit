22fffccdfaa679fbd2f3a73373ea29aac7560b8c
ShopSheep <137586735+ShopSheep@users.noreply.github.com>
2023-06-29 15:19:08 -0700

    Update testing.py
    extended limit

--- testing.py
+++ testing.py
@@ -6,7 +6,7 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get(
-    "https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1]
+    "https://itunes.apple.com/search?entity=song&limit=100&term=" + sys.argv[1]
 )
 
 print(json.dumps(response.json(), indent=2))
